9d3d3bfb3667dd561c73db1ff4f2950747265ea0
Gonzalo Rafuls <grafuls@gmail.com>
2026-02-03 22:00:41 +0100

    fix: unified versioning for semantic-release

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import setuptools
+import re
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(here, "src", "badfish", "__init__.py")
+
+with open(version_file, "r", encoding="utf-8") as f:
+    content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        current_version = match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in src/badfish/__init__.py")
 
 setuptools.setup(
-    version="1.0.7"
+    version=current_version
 )
